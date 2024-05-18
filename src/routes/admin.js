3393ca102162337bc39eac3833220bd28ecda381
BuiPhuongDongQuan <s3975939@rmit.edu.vn>
2024-05-18 12:32:22 +0700

    done-link-page-admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,8 +1,7 @@
 import express from "express";
 export const adminRouter = express.Router();
-import {Product} from "../models/productSchema.js";
+// import {Product} from "../models/productSchema.js";
 
-adminRouter.get('/admin', (req, res) => {
-    console.log("Redirecting to my account page")
-    res.render("admin_page")
+adminRouter.get("/", (req, res) => {
+    res.render('admin_page')
 })
